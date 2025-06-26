72d647fa191c27c2f0eef4eace8d5dd1916bb58a
lokendra-singh-dhirawat <lokendra20048@gmail.com>
2025-06-26 16:41:57 +0000

    add error handler at root of the index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import gameRtr from "./routes/gameRtr";
 import morgan from "morgan";
+import { error } from "winston";
+import errorHandler from "./middleware/errorHandler";
 
 dotenv.config();
 
@@ -12,6 +14,7 @@ app.set("json spaces", 5);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", gameRtr);
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
