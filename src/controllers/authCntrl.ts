d74efb58b88e57d38d6318f76e91b9769ae01cf0
lokendra-singh-dhirawat <lokendra20048@gmail.com>
2025-06-30 13:36:17 +0000

    add login method for authentication

---
+++ src/controllers/authCntrl.ts
@@ -0,0 +1,129 @@
+import type { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { PrismaClient } from "@prisma/client";
+import logger from "../config/logger";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import crypto from "crypto";
+import { AppError, ConflictError, UnauthorizedError } from "../utils/error";
+
+const prisma = new PrismaClient();
+
+class AuthCntrl {
+  private JWT_SECRET: string;
+  private BCRYPT_SALT_ROUNDS: number;
+  private ACCESS_TOKEN_EXPIRATION_TIME: number = 900;
+  private REFRESH_TOKEN_EXPIRATION_TIME: number = 7;
+
+  constructor() {
+    this.JWT_SECRET = process.env.JWT_SECRET!;
+    this.BCRYPT_SALT_ROUNDS = 10;
+  }
+
+  private genrateRefreshToken = (): string => {
+    return crypto.randomBytes(64).toString("hex");
+  };
+  public register = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { email, password, name } = req.body;
+
+      const hashedPassword = await bcrypt.hash(
+        password,
+        this.BCRYPT_SALT_ROUNDS
+      );
+
+      const newUser = await prisma.user.create({
+        data: {
+          email,
+          password: hashedPassword,
+          name,
+        },
+        select: {
+          id: true,
+          email: true,
+          name: true,
+          createdAt: true,
+        },
+      });
+      logger.info(`User registred and successfully: ${newUser.email}`);
+      res.status(201).json({
+        message: "User registered successfully",
+        user: newUser,
+      });
+    } catch (error: any) {
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === "P2002"
+      ) {
+        throw new ConflictError("Email already registered.");
+      }
+      if (error instanceof AppError) {
+        throw error;
+      }
+      logger.error(`Error during registration: ${error.message}`, { error });
+      throw new AppError(
+        "An unexpected error occurred during registration.",
+        500
+      );
+    }
+  };
+
+  public login = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { email, password } = req.body;
+
+      const user = await prisma.user.findUnique({
+        where: { email },
+      });
+      if (!user) {
+        throw new UnauthorizedError(
+          "Invalid credentials",
+          "INVALID_CREDENTIALS"
+        );
+      }
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordValid) {
+        throw new UnauthorizedError(
+          "Invalid credentials",
+          "INVALID_CREDENTIALS"
+        );
+      }
+
+      const accessToken = jwt.sign(
+        { id: user.id, email: user.email, role: user.role },
+        this.JWT_SECRET,
+        {
+          expiresIn: this.ACCESS_TOKEN_EXPIRATION_TIME,
+        }
+      );
+
+      const newRefreshToken = this.genrateRefreshToken();
+      const refreshTokenExpiresAt = new Date(
+        Date.now() + this.REFRESH_TOKEN_EXPIRATION_TIME * 24 * 60 * 60 * 1000
+      );
+
+      await prisma.user.update({
+        where: { id: user.id },
+        data: {
+          refreshToken: newRefreshToken,
+          refreshTokenExpiresAt: refreshTokenExpiresAt,
+        },
+      });
+
+      logger.info(`User logged in: ${user.email} and issued tokens.`);
+
+      res.status(200).json({
+        accessToken,
+        refreshToken: newRefreshToken,
+        expiresIn: this.ACCESS_TOKEN_EXPIRATION_TIME,
+      });
+    } catch (error: any) {
+      if (error instanceof AppError) {
+        throw error;
+      }
+      logger.error(`Error during login: ${error.message}`, { error });
+      throw new AppError("An unexpected error occurred during login.", 500);
+    }
+  };
+}
